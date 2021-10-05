00241281b90f5a0c2d58413a847b96de9460582e
yoshi0105 <hjmkd2738@gmail.com>
2021-10-05 13:15:54 +0900

    コミットし直し

--- htdocs/drink/tool.php
+++ htdocs/drink/tool.php
@@ -9,9 +9,15 @@ $stock = '';
 $create_at = '';
 $update_at = '';
 $img = '';
+$drink_list = [];
 $err_msg = [];
+$drink_id;
 //$link = '';
 $link = mysqli_connect($host, $user_name, $passwd, $dbname);
+
+
+print $_POST['public'];
+print $_POST['stock'];
 
 if ($link = mysqli_connect($host, $user_name, $passwd, $dbname)) {
 
@@ -40,8 +46,22 @@ if ($link = mysqli_connect($host, $user_name, $passwd, $dbname)) {
             }
 
 
+            $sql = 'SELECT MAX(drink_id) as max_id FROM drink_info;';
+
+            // クエリ実行
+            if ($result = mysqli_query($link, $sql)) {
+                $i = 0;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $drink_id = htmlspecialchars($row['max_id'], ENT_QUOTES, 'UTF-8');
+                }
+            } else {
+                $err_msg[] = 'SQL失敗:' . $sql;
+                print 'select失敗'.$sql;
+            }
+
             $name = $_POST['name'];
             $price = $_POST['price'];
+            $stock = $_POST['stock'];
             $public = $_POST['public'];
             $date = date('Y-m-d H:i:s');
 
@@ -80,14 +100,32 @@ if ($link = mysqli_connect($host, $user_name, $passwd, $dbname)) {
             $img = $_FILES['userfile']['tmp_name'];
 
 
-            $sql = "INSERT INTO `drink_info`(`name`, `price`, `created_at`, `update_at`, `public`, `image`) 
-                    VALUES ('".$name."','".$price."','".$date."','".$date."','".$public."','".$img_data."');";
+            $sql = "INSERT INTO `drink_info`(`drink_id`, `name`, `price`, `create_at`, `update_at`, `public`, `image`) 
+                    VALUES ('".($drink_id + 1)."', '".$name."','".$price."','".$date."','".$date."','".$public."','".$img_data."');";
             
             print 'インサートする'.$sql;
             //print $img;
             if (mysqli_query($link, $sql) !== TRUE) {
-                $err_msg[] = 'order_detail_table: insertエラー:' . $sql;
-            } 
+                $err_msg[] = 'drink_info: insertエラー:' . $sql;
+            }
+
+            $sql = "INSERT INTO `drink_history`(`drink_id`, `bought_at`) VALUES ('".($drink_id + 1)."', '".$date."');";
+    
+            print 'インサートする'.$sql;
+            //print $img;
+            if (mysqli_query($link, $sql) !== TRUE) {
+                $err_msg[] = 'drink_history: insertエラー:' . $sql;
+            }
+
+
+            $sql = "INSERT INTO `drink_stock`(`drink_id`,`stock`, `create_at`, `update_at`) 
+                    VALUES ('".($drink_id + 1)."', '".$stock."','".$date."','".$date."');";
+            
+            print 'インサートする'.$sql;
+            //print $img;
+            if (mysqli_query($link, $sql) !== TRUE) {
+                $err_msg[] = 'drink_stock: insertエラー:' . $sql;
+            }
             print '<img src="<?='.$img.'?>">';
         }
     }
@@ -99,7 +137,9 @@ if ($link = mysqli_connect($host, $user_name, $passwd, $dbname)) {
     }
 
 
-    $sql = 'SELECT drink_id,name, price, image FROM drink_info';
+    $sql = 'SELECT drink_info.drink_id,drink_info.name, drink_info.price, drink_info.public, drink_info.image, drink_stock.stock
+    FROM drink_info INNER JOIN drink_stock on drink_info.drink_id = drink_stock.drink_id;';
+    //print 'select失敗'.$sql;
     // クエリ実行
     if ($result = mysqli_query($link, $sql)) {
         $i = 0;
@@ -107,14 +147,14 @@ if ($link = mysqli_connect($host, $user_name, $passwd, $dbname)) {
             $drink_list[$i]['id'] = htmlspecialchars($row['drink_id'], ENT_QUOTES, 'UTF-8');
             $drink_list[$i]['name'] = htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8');
             $drink_list[$i]['price'] = htmlspecialchars($row['price'], ENT_QUOTES, 'UTF-8');
+            $drink_list[$i]['public'] = htmlspecialchars($row['public'], ENT_QUOTES, 'UTF-8');
             $drink_list[$i]['image'] = htmlspecialchars($row['image'], ENT_QUOTES, 'UTF-8');
-            //$drink_id = htmlspecialchars($row['drink_id'], ENT_QUOTES, 'UTF-8');
-            //$drink_img = htmlspecialchars($row['image'], ENT_QUOTES, 'UTF-8');
+            $drink_list[$i]['stock'] = htmlspecialchars($row['stock'], ENT_QUOTES, 'UTF-8');
             $i++;
         }
     } else {
         $err_msg[] = 'SQL失敗:' . $sql;
-        print 'select失敗';
+        print 'select失敗'.$sql;
     }
 
     //print "\n";
@@ -134,6 +174,7 @@ for ($i = 0; $i < count($err_msg); $i++) {
     <meta charset="utf-8">
     <title></title>
     <style></style>
+    <link rel="stylesheet" href="drink.css">
 </head>
 <body>
 
@@ -154,23 +195,52 @@ for ($i = 0; $i < count($err_msg); $i++) {
             <input type="hidden" name="sql_type" value="insert">
             <div><input type="submit" value="■□■□■商品追加■□■□■"></div>
         </form>
-
-
-        <form method="post">
-           <ul>
-            <?php       foreach ($drink_list as $item) { ?>
-               <li>
-                   <!-- <span><?php print $item['id']; ?></span> 
-                   <span><?php print $item['image']; ?></span> -->
-                   <span><?php print '<img src="'.$item['image'].'">'; ?></span>
-                   <span><?php print $item['name']; ?></span>
-                   <span><?php print $item['price']; ?></span>
-                   <!-- <span><div><label><input type="text" name="stock" value="<?php print $item['image']; ?>"></label></div></span> -->
-               </li>
-            <?php    } ?>
-            <!-- <img src="uploads/2c9a0b78f71f73643638a3a64ec96704.jpg.jpg"> -->
-           </ul>
-       </form>
+           <table>
+                <tr>
+                    <td>商品画像</td>
+                    <td>商品名</td>
+                    <td>価格</td>
+                    <td>在庫</td>
+                    <td>ステータス</td>
+                </tr>
+                <?php       foreach ($drink_list as $item) { ?>
+                    <tr>
+                        <td><?php print '<img src="'.$item['image'].'">'; ?></td>
+                        <td><?php print $item['name']; ?></td>
+                        <td><?php print $item['price']; ?></td>
+                        <td>
+                        <form method="post">
+                            <input type="text" name="stock" value="<?php print $item['stock']; ?>">
+                            <input type="hidden" name="sql_type" value="update">
+                            <input type="submit" value="変更">
+                        </form>
+                        </td>
+                        <?php
+                        if ( $item['public'] === 1 ){
+                        ?>
+                            <td>
+                                <form method="post">
+                                    <input type="submit" value="公開→非公開">
+                                    <input type="hidden" name="public" value=0>
+                                    <input type="hidden" name="sql_type" value="update">
+                                </form>
+                            </td>
+                        <?php
+                        } else { 
+                        ?>
+                            <td>
+                                <form method="post">
+                                    <input type="submit" value="非公開→公開">
+                                    <input type="hidden" name="public" value=1>
+                                    <input type="hidden" name="sql_type" value="update">
+                                </form>
+                            </td>
+                        <?php
+                        }
+                        ?>
+                    </tr>
+                <?php    } ?>
+            </table>
 
     </section>
 </body>
